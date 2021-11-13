53732a210865e741ed27be87e4c6e12b3829b0de
jakdev047 <jakfreelancer047@gmail.com>
2021-11-13 22:18:53 +0600

    top Navigation

---
+++ src/common/FilterSearch.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Clear, Search } from "@mui/icons-material";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
+
+export default function FilterSearch({
+  placeholder,
+  value,
+  setValue,
+  cancelHandler,
+  width,
+  crossClassName,
+}) {
+  return (
+    <div
+      style={{
+        border: "1px solid #E5E5E6",
+        width: width || "392px",
+        borderRadius: "8px",
+        marginRight: "10px",
+        display: "flex",
+        alignItems: "center",
+        height: "40px",
+        background: "#F2F2F3",
+        padding: "0 10px 0 0",
+      }}
+      component="form"
+    >
+      <IconButton
+        type="button"
+        sx={{
+          p: "8px",
+          "&.MuiButtonBase-root": {
+            "&:hover": {
+              backgroundColor: "transparent",
+            },
+          },
+          "&.MuiTouchRipple-root": {
+            "&:focus": {
+              backgroundColor: "transparent",
+            },
+          },
+        }}
+        aria-label={placeholder ? placeholder : "search"}
+      >
+        <Search
+          sx={{
+            "&.MuiSvgIcon-root": {
+              color: "#88898E",
+            },
+          }}
+        />
+      </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1, width: 300 }}
+        placeholder={placeholder ? placeholder : "Search"}
+        inputProps={{ "aria-label": placeholder ? placeholder : "search" }}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.keyCode === 13) {
+            setValue(e.target.value);
+          }
+        }}
+      />
+      {(value.length || 0) > 0 && (
+        <span
+          className={crossClassName ? `pointer ${crossClassName}` : "pointer"}
+          onClick={() => cancelHandler()}
+        >
+          <Clear
+            sx={{
+              "&.MuiSvgIcon-root": {
+                color: "#88898E",
+              },
+            }}
+          />
+        </span>
+      )}
+    </div>
+  );
+}
--- src/layout/topNavigation/TopNavigation.jsx
+++ src/layout/topNavigation/TopNavigation.jsx
@@ -1,35 +1,93 @@
-import React from 'react';
-import ActionMenu from '../../common/ActionMenu';
-import { RemoveRedEyeOutlined, EditOutlined } from "@mui/icons-material";
+import React from "react";
+import * as Yup from "yup";
+import { Formik, Form } from "formik";
+import { ArrowDropDown, NotificationsNoneOutlined } from "@mui/icons-material";
+import FilterSearch from "../../common/FilterSearch";
+import profileImg from "../../assets/img/profileImg.png";
+
+const initData = {
+  searchString: "",
+};
+
+const validationSchema = Yup.object({});
 
 export default function TopNavigation() {
-   return (
-      <>
-         <div className="top-navigation">
-            <div className="logo-wrapper">
-            <ActionMenu
-      options={[
-         {
-            value: 1,
-            label: 'View',
-            icon: <RemoveRedEyeOutlined sx={{ marginRight: "10px" }} />,
-            onClick: () => {
-               alert('View One');
-            }
-         },
-         {
-            value: 2,
-            label: 'Edit',
-            icon: <EditOutlined sx={{ marginRight: "10px" }} />,
-            onClick: () => {
-               alert('Edit One');
-            }
-         }
-      ]}
-   />
-            </div>
-            <div className="top-menu"></div>
-         </div>
-      </>
-   );
+  const saveHandler = (values) => {
+    console.log(values);
+  };
+  return (
+    <>
+      <Formik
+        enableReinitialize={true}
+        initialValues={initData}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          saveHandler(values, () => {
+            resetForm(initData);
+          });
+        }}
+      >
+        {({
+          handleSubmit,
+          resetForm,
+          values,
+          errors,
+          touched,
+          setFieldValue,
+          handleBlur,
+          isValid,
+        }) => (
+          <>
+            <Form onSubmit={handleSubmit}>
+              <div className="top-navigation">
+                <div className="top-navigation-left">
+                  <div className="top-drop-menu">
+                    <span className="txt">Starbucks</span>
+                    <span className="icon">
+                      <ArrowDropDown />
+                    </span>
+                  </div>
+                </div>
+                <div className="top-menu">
+                  <div>
+                    <FilterSearch
+                      value={values?.searchString}
+                      setValue={(value) => {
+                        setFieldValue("searchString", value);
+                      }}
+                      cancelHandler={() => setFieldValue("searchString", "")}
+                      handleClick={() => {}}
+                    />
+                  </div>
+                  <div className="flex items-center justify-items-center">
+                    <div className="top-user pointer">
+                      <span>
+                        <NotificationsNoneOutlined
+                          sx={{
+                            color: "action.active",
+                            zIndex: 1,
+                          }}
+                        />
+                      </span>
+                    </div>
+                    <div className="top-user pointer">
+                      <div className="profile-menu-img">
+                        <img src={profileImg} alt="iBOS" className="border-0" />
+                      </div>
+                    </div>
+                    <div className="top-drop-menu">
+                      <span className="txt">John Doe</span>
+                      <span className="icon">
+                        <ArrowDropDown />
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Form>
+          </>
+        )}
+      </Formik>
+    </>
+  );
 }
